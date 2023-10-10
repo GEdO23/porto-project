69f0dddf7deff5b51636a2526d9f2bb25f367a62
GEdO23 <rm99632@fiap.com.br>
2023-10-10 11:40:44 -0300

    Definindo e organizando variaveis sass em suas respectivas pastas e arquivos.

--- src/routers/Home/Home.jsx
+++ src/routers/Home/Home.jsx
@@ -3,6 +3,8 @@ import "./Home.scss";
 export default function Home() {
   return (
     <div className="homeContainer">
+      <h1>Hello World</h1>
+      <h2>Hello World</h2>
       <button>hello world</button>
     </div>
   )
