fda8417ee8f2a0dfa4956803b6a4d84fb83c975a
GEdO23 <rm99632@fiap.com.br>
2023-10-16 09:11:38 -0300

    Adcionando estilização ao ImageSlider.

--- src/components/ImageSlider/imageSlider.jsx
+++ src/components/ImageSlider/imageSlider.jsx
@@ -1,7 +1,27 @@
-export default function ImageSlider() {
+import { useState } from "react";
+
+export default function ImageSlider({slides}) {
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  const sliderStyles = {
+    height: '100%',
+    position: 'relative',
+  };
+
+  const slideStyles = {
+    width: '100%',
+    height: '100%',
+    borderRadius: '10px',
+    backgroundPosition: 'center',
+    backgroundSize: 'cover',
+    backgroundImage: `url(${slides[currentIndex].url})`,
+  };
+
   return (
   <>
-  
+  <div style={sliderStyles}>
+    <div style={slideStyles} />
+  </div>
   
   </>
   );
