5f370130308ca72294cb5b6716fe12fedd93ad17
GEdO23 <rm99632@fiap.com.br>
2023-10-16 09:54:05 -0300

    Adcionando funcionalidades.

--- src/components/ImageSlider/imageSlider.jsx
+++ src/components/ImageSlider/imageSlider.jsx
@@ -1,27 +1,96 @@
 import { useState } from "react";
 
-export default function ImageSlider({slides}) {
+export default function ImageSlider({ slides }) {
   const [currentIndex, setCurrentIndex] = useState(0);
 
   const sliderStyles = {
-    height: '100%',
-    position: 'relative',
+    height: "100%",
+    position: "relative",
+  };
+
+  const dotsContainerStyles = {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: 'end',
+    backdropFilter: 'invert(80%)'
   };
 
   const slideStyles = {
-    width: '100%',
-    height: '100%',
-    filter: 'brightness(75%) blur(.8px)',
-    backgroundPosition: 'center',
-    backgroundSize: 'cover',
+    width: "100%",
+    height: "100%",
+    backgroundPosition: "center",
+    backgroundSize: "cover",
     backgroundImage: `url(${slides[currentIndex].url})`,
+    ...dotsContainerStyles
+  };
+
+  const leftArrowStyles = {
+    position: "absolute",
+    top: "50%",
+    transform: "translate(0, -50%)",
+    left: "32px",
+    fontSize: "45px",
+    color: "#fff",
+    zIndex: 1,
+    cursor: "pointer",
+  };
+
+  const rightArrowStyles = {
+    position: "absolute",
+    top: "50%",
+    transform: "translate(0, -50%)",
+    right: "32px",
+    fontSize: "45px",
+    color: "#fff",
+    zIndex: 1,
+    cursor: "pointer",
+  };
+  // ⃝ ●
+
+
+  const dotStyles = {
+    margin: "0 3px",
+    cursor: "pointer",
+    fontSize: "20px",
+  };
+
+  const goToPrevious = () => {
+    const isFirstSlide = currentIndex === 0;
+    const newIndex = isFirstSlide ? slides.length - 1 : currentIndex - 1;
+    setCurrentIndex(newIndex);
+  };
+
+  const goToNext = () => {
+    const isLastSlide = currentIndex === slides.length - 1;
+    const newIndex = isLastSlide ? 0 : currentIndex + 1;
+    setCurrentIndex(newIndex);
+  };
+
+  const goToSlide = (slideIndex) => {
+    setCurrentIndex(slideIndex);
   };
 
   return (
-  <>
-  <div style={sliderStyles}>
-    <div style={slideStyles} />
-  </div>
-  </>
+    <>
+      <div style={sliderStyles}>
+        <div style={leftArrowStyles} onClick={goToPrevious}>
+          ⇦
+        </div>
+        <div style={rightArrowStyles} onClick={goToNext}>
+          ⇨
+        </div>
+        <div style={slideStyles}>
+          {slides.map((slide, slideIndex) => (
+            <div
+              key={slideIndex}
+              style={dotStyles}
+              onClick={() => goToSlide(slideIndex)}
+            >
+              ●
+            </div>
+          ))}
+        </div>
+      </div>
+    </>
   );
 }
