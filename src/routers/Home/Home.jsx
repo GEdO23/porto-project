e1956a72753b01982069694a5270bc602c98002b
GEdO23 <rm99632@fiap.com.br>
2023-10-16 09:11:17 -0300

    Adcionando estilização e urls para os slides.

--- src/routers/Home/Home.jsx
+++ src/routers/Home/Home.jsx
@@ -2,13 +2,19 @@ import ImageSlider from "../../components/ImageSlider/imageSlider";
 
 export default function Home() {
   const slides = [
-    { url: "", title: "" },
-    { url: "", title: "" },
-    { url: "", title: "" },
+    { url: "/public/imgs/img-bike.jpg", title: "Contrato de Bike" },
+    { url: "/public/imgs/img-game.jpg", title: "Gamificacao" },
+    { url: "/public/imgs/img-corp.jpg", title: "Equipe" },
   ];
 
+  const containerStyles = {
+    width: '500px',
+    height: '280px',
+    margin: '0 auto'
+  };
+
   return (
-    <div>
+    <div style={containerStyles}>
       <ImageSlider slides={slides} />
     </div>
   );
